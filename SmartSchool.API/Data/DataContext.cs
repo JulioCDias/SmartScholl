445bfa33c2749f22b701f37ba17c19720524aca1
Julio Cesar <juliozulu2008@gmail.com>
2025-05-29 21:40:33 -0300

    Iniciando a Seção de Entity Framework

--- SmartSchool.API/Data/DataContext.cs
+++ SmartSchool.API/Data/DataContext.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SmartSchool.API.Models;
 
 namespace SmartSchool.API.Data
 {
     public class DataContext : DbContext
     {
-
+        public DbSet<Aluno> alunos { get; set; }
+        public DbSet<Professor> professores { get; set; }
+        public DbSet<Disciplina> disciplinas { get; set; }
+        public DbSet<AlunoDisciplina> alunoDisciplinas { get; set; }
     }
 }
